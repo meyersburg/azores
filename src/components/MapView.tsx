ad12b46106c264c8bad46f10abe1d0eb29aa6ba0
Jason Meyersburg <jason.d.meyersburg@gmail.com>
2026-04-15 15:07:34 -0400

    Fix map resetting on filter tap: remove MapInit

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import type { Poi } from '../types'
 import { tagColor } from '../types'
@@ -28,11 +28,6 @@ function makeIcon(color: string) {
   })
 }
 
-function MapInit() {
-  const map = useMap()
-  map.setView([37.79, -25.47], 11)
-  return null
-}
 
 interface Props {
   pois: Poi[]
@@ -45,7 +40,6 @@ export function MapView({ pois }: Props) {
       center={[37.79, -25.47]}
       zoom={11}
     >
-      <MapInit />
       <TileLayer
         attribution='&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=${import.meta.env.VITE_MAPTILER_KEY}`}
